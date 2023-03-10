222c93e668659a947cf02e4833e0b2bee2a92626
Andrei <andrei.pata@wtzconsult.com>
2023-03-10 17:59:54 +0200

    Changed request body of create article method

--- src/main/java/com/sup/core/models/support/SupportArticleRequestModel.java
+++ src/main/java/com/sup/core/models/support/SupportArticleRequestModel.java
@@ -9,7 +9,6 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class SupportArticleRequestModel {
-  Long id;
   Long subcategoryId;
   String title;
   String content;
